d170d743b158a8283994183417591993f6ba5b22
Jay <j.kye71@gmail.com>
2023-12-05 22:06:39 -0800

    solved 4th question

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ document.getElementById("container");
 document.querySelector("#container");
 
 // 3. Select all of the list items with a class of “second”.
-document.getElementsByClassName("second")
+document.getElementsByClassName("second");
 
-document.querySelectorAll(".second")
-
+document.querySelectorAll(".second");
 
 // 4. Select a list item with a class of third, but only the list item inside of the ol tag.
+ const q4 = document.querySelectorAll(".third");
+ q4[1];
+ 
 // 5. Give the section with an id of container the text “Hello!”.
 // 6. Add the class main to the div with a class of footer.
 // 7. Remove the class main on the div with a class of footer.
