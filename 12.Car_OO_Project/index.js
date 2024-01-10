c9b8c465254780cb2f2ac137556f6b52b51995b0
Jay Kye <67670763+jay-kye@users.noreply.github.com>
2024-01-10 12:38:28 -0800

    fixed codes
    updated codes to return strings, and to inherit parents class properly.

--- 12.Car_OO_Project/index.js
+++ 12.Car_OO_Project/index.js
@@ -8,50 +8,50 @@ class Vehicle {
         this.year = year;
     }
     honk(){
-        console.log(`Beep Beep!`);
+        return "Beep Beep!";
     }
     toString(){
-        console.log(`The vehicle is a ${this.make} ${this.model} from ${this.year}.`)
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
     }
 }
 
 let myFirstVehicle = new Vehicle("Tesla", "Model Y", 2021);
 myFirstVehicle.honk();
 myFirstVehicle.toString();
-console.log('===========divider===========')
 
 /* Part two
  * Class Car inherited Class Vehicle and has numWheels method that belong to Car class.
  */
 class Car extends Vehicle{
-    numWheels(){
-        console.log(4);
-    }
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 4;
+    } 
+    
 }
 
 let myFirstCar = new Car("Tesla", "Model Y", 2021);
-myFirstCar.numWheels();
-console.log('===========divider===========')
+myFirstCar.numWheels;
 
 /* Part Three
  * Class Motorcycle inherited Class Vehicle and has numWheels method that belong to Motorcycle class.
  */
 
 class Motorcycle extends Vehicle{
-    numWheels(){
-        console.log(2);
-    }
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    } 
     revEngine(){
-        console.log(`Vrrrrroooooooom!!!`);
+        return "Vrrrrroooooooom!!!";
     }
 }
 
 let myFirstMotocycle = new Motorcycle("BMW", "Rider", 2022);
 myFirstMotocycle.toString();
 myFirstMotocycle.honk();
-myFirstMotocycle.numWheels();
+myFirstMotocycle.numWheels;
 myFirstMotocycle.revEngine();
-console.log('===========divider===========')
 
 /* Part Four
 * Class Garage that 
@@ -63,26 +63,26 @@ class Garage{
     }
     add(enteringVehicle){
         if(!(enteringVehicle instanceof Vehicle)){
-            console.log("Only vehicles are allowed in here!")
+            return "Only vehicles are allowed in here!";
             // it checks the parameter if it's a vehicle or not.
         } else if(this.vehicle.length >= this.capacity){
-            console.log("Sorry, we're full.");
+            return "Sorry, we're full.";
             // it checks the capacity if it's full or not.
         } else{
             this.vehicle.push(enteringVehicle);
-            console.log("Vehicle added!");
+            return "Vehicle added!";
             // after checking those 2 conditions, finally add a vehicle in an array of garage object.
         }
     }   
 }
 
-let garage = new Garage(2);
-// new garage created with the capacity of 2
-garage.add(new Car('Tesla', 'Cyberturck', 2024));
-// vehicle added successfully
-garage.add(new Car('Tesla', 'Model S', 2024));
-// vehicle added successfully
-garage.add(new Car('Tesla', 'Model Y', 2024));
-// now garage is full, can't add any more
-garage.add('taco');
-// this is not a vehicle, and it can't enter the garage.
+// let garage = new Garage(2);
+// // new garage created with the capacity of 2
+// garage.add(new Car('Tesla', 'Cyberturck', 2024));
+// // vehicle added successfully
+// garage.add(new Car('Tesla', 'Model S', 2024));
+// // vehicle added successfully
+// garage.add(new Car('Tesla', 'Model Y', 2024));
+// // now garage is full, can't add any more
+// garage.add('taco');
+// // this is not a vehicle, and it can't enter the garage.
